ba0b7f1ac3560e3b835c7989c7c0ad37a54cac46
Bruno Morisson <morisson@genhex.org>
2019-01-03 16:00:56 +0000

    fix renew

--- certbot/renew.sh
+++ certbot/renew.sh
@@ -8,7 +8,5 @@ fi
 DOMAIN=$1
 
 echo Running certbot for domain $DOMAIN
-echo
-read -p "Press any key to continue, or CTRL-C to bail out" var_p
 
 docker run --rm --name certbot --hostname certbot -ti  -p 53:53/udp -p 53:53 -v $PWD/certbot/logs:/var/log/letsencrypt -v $PWD/certbot/letsencrypt:/etc/letsencrypt/ certbot-burp renew --force-renewal
