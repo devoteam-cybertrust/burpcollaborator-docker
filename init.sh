196e3b7de659a1b9fbbd40a5671e7126a2762bb5
Bruno Morisson <morisson@genhex.org>
2019-01-04 09:23:22 +0000

    auto basedir fix 2

--- init.sh
+++ init.sh
@@ -51,6 +51,6 @@ docker build  -t burp burp && \
 /bin/mv ./init.sh ./init.sh_has_been_run && \
 /bin/chmod 000 ./init.sh_has_been_run && \
 /bin/sed -i "s/__DOMAIN__/$DOMAIN/g" ./certbot/certificaterenewal.sh && \
-/bin/sed -i "s/__BASEDIR__/$PWD/g" ./certbot/certificaterenewal.sh && \
+/bin/sed -i "s#__BASEDIR__#$PWD#g" ./certbot/certificaterenewal.sh && \
 
 echo Burp is now running with the letsencrypt certificate for domain *.$DOMAIN
