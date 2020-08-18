c929acdd22670555618dd70acf3702c7cf3601e1
Bruno Morisson <morisson@genhex.org>
2020-08-18 16:48:46 +0100

    randomize metrics path. closes #9

--- init.sh
+++ init.sh
@@ -37,6 +37,7 @@ fi
 
 DOMAIN=$1
 IP=$2
+METRICS=`LC_CTYPE=C tr -dc A-Za-z0-9 < /dev/urandom | fold -w 10 | head -1`
 
 echo Initialization to be done with domain *.$1 and public ip $2 && \
 read -p "Press any key to continue, or CTRL-C to bail out" var_p  && \
@@ -47,10 +48,14 @@ docker build  -t burp burp && \
 /bin/cp -f ./certbot/letsencrypt/live/$DOMAIN/*.pem ./burp/keys && \
 /bin/sed -i "s/DOMAIN/$DOMAIN/g" ./burp/conf/burp.config && \
 /bin/sed -i "s/IP/$IP/g" ./burp/conf/burp.config && \
+/bin/sed -i "s/jnaicmez8/$METRICS/g" ./burp/conf/burp.config && \
 ./burp/run.sh && \
 /bin/mv ./init.sh ./init.sh_has_been_run && \
 /bin/chmod 000 ./init.sh_has_been_run && \
 /bin/sed -i "s/__DOMAIN__/$DOMAIN/g" ./certbot/certificaterenewal.sh && \
 /bin/sed -i "s#__BASEDIR__#$PWD#g" ./certbot/certificaterenewal.sh && \
 
-echo Burp is now running with the letsencrypt certificate for domain *.$DOMAIN
+echo 
+echo SUCCESS! Burp is now running with the letsencrypt certificate for domain *.$DOMAIN
+echo
+echo Your metrics path was set to $METRICS. Change addressWhitelist to access it remotely.
